a2085652748f1c6dcc9beeb6c641bad8bb7cd451
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-04-15 13:55:01 +0600

    first commit. project created

---
+++ src/main/java/com/projects/naziat/ultra/UltraApplication.java
@@ -0,0 +1,13 @@
+package com.projects.naziat.ultra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/naziat/ultra/UltraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.naziat.ultra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
